71d0a021a5d66b6b40650bf1b5849a67e7973d28
unknown <medamirag@gmail.com>
2022-12-09 12:16:04 +0100

    chapitre 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AfterViewChecked, ChangeDetectorRef } from '@angular/core'
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'templateDrivenForm';
+
+  statuts=['Célibataire','Marié(e)','Divorcé(e)']
+
+  user:User={email:"test@gmail.com",nationalite:"TUNISIEN",nom:"Ahmed",statut:"Célibataire",termes:true}
+  
 }
---
+++ src/app/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+    nom:string;
+    email:string;
+    statut:string;
+    nationalite:string;
+    termes:boolean
+}
